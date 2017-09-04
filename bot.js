2e79468f53c7201ef77fb2b8aa9b28cd1b2be8a8
lcr1997lcr <luis.c.ruiz97@gmail.com>
2017-09-04 08:21:18 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,7 +14,7 @@ function respond() {
       botRegexGian = /^\/gian/; botRegexScrotes = /^\/scrotes/; botRegexShaki = /^\/shaki/;
       botRegexDaf = /^\Dafuq/; botRegexMA32 = /^\/pending/; botRegexTrade = /^\/trade/; 
       botRegexWDF = /^\Salinger/; botRegexWDF2 = /^\Eric/ ; botRegexBoi = /^\Boi/; botRegexCuevs = /^\Luis Cuevas/; botRegexCuevs2 = /^\Luis cuevas/;
-      botRegexJose = /^\Jose/; botRegexFBM = /^\Feels bad/; botRegexHey = /^\Hey/;
+      botRegexJose = /^\Jose/; botRegexFBM = /^\Feels bad/; 
       siege1 = 'https://i.groupme.com/350x419.png.adc8c73a6c1547e0a9e04320296329f8'; siege2 = 'https://i.groupme.com/1279x752.jpeg.aa5d0401e0df495bba4b4e09dc5a6bd7'
       siege3 = 'https://i.groupme.com/960x960.png.006e180e05d841c6a2962e844bf1e6fd';
   var teamAb = ["NE","NO","ARI","PHI","CLE","TEN","OAK","DAL","IND","SEA","CIN","PIT","JAC"
